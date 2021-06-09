ecc4df68b5b8ed3090a2e3b6390671bf26032023
RaghavPandit <rp20020918@gmail.com>
2021-06-09 19:10:35 +0530

    readding sketch.js and gitigonre

--- sketch.js
+++ sketch.js
@@ -1,9 +1,16 @@
+var r1 = 100;
+var r2 = 100;
+var m1 = 10;
+var m2 = 10;
+
+
 function setup(){
     createCanvas(400,400);
-    background(212);
 
 }
 
 function draw(){
-
+    background(255);
+    stroke(0);
+    strokeWeight(2);
 }
